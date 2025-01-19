ecf0a20f9e2b01ae80fb910166f438d0a97eba7c
danclarkdev <80196114+danclarkdev@users.noreply.github.com>
2025-01-19 19:57:59 +0000

    initial commit

---
+++ components/ContentfulEntry.vue
@@ -0,0 +1,16 @@
+<template>
+    <slot
+        :="{
+            attrs: {
+                'data-contentful-entry-id': props.entryId,
+            },
+        }"
+    />
+
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    entryId: string;
+}>();
+</script>
---
+++ pages/posts.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-for="post in posts" :key="post.id">
+        <ContentfulEntry :entry-id="post.id" #="{ attrs }">
+            <h1 :="attrs">{{ post.title }}</h1>
+        </ContentfulEntry>
+    </div>
+</template>
+
+<script setup lang="ts">
+const posts = [
+    {
+        id: 'aaaaa',
+        title: 'A Post',
+    },
+    {
+        id: 'bbbbb',
+        title: 'B Post',
+    },
+    {
+        id: 'ccccc',
+        title: 'C Post',
+    },
+];
+</script>
